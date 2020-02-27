f114e1508336ed66c1bfd48425dddf6ec7a6a9c2
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2020-02-27 14:58:33 -0800

    connect mongodb

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const mongoURI = config.get("mongoURI");
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.listen(PORT, () => console.log("App is listening on port: " + PORT));
