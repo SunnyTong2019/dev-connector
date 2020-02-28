42c39fac90fdf0849d40d06643a21d31d9ede948
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2020-02-28 09:36:47 -0800

    set up route files with express router

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const mongoURI = config.get("mongoURI");
+const authRoute = require("./routes/api/auth");
+const profileRoute = require("./routes/api/profile");
+const postsRoute = require("./routes/api/posts");
 
 const app = express();
 
@@ -10,6 +13,14 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/posts", postsRoute);
+
+app.get("/", function(req, res) {
+  res.send("Hello World");
 });
 
 app.listen(PORT, () => console.log("App is listening on port: " + PORT));
