6c2289dc78acc66beb74a990bff6b239a0171bee
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2020-02-28 10:49:31 -0800

    register user route: check email existence, hash password and save to database

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const config = require("config");
+const mongoURI = config.get("mongoURI");
 const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
 
 router.get("/", function(req, res) {
   res.send("auth route");
@@ -14,9 +26,14 @@ router.post(
   [
     check("name")
       .not()
-      .isEmpty(),
-    check("email").isEmail(),
-    check("password").isLength({ min: 6 })
+      .isEmpty()
+      .withMessage("name is required"),
+    check("email")
+      .isEmail()
+      .withMessage("Please provide valid email address"),
+    check("password")
+      .isLength({ min: 6 })
+      .withMessage("password must be at least 6 chars long")
   ],
   function(req, res) {
     const errors = validationResult(req);
@@ -24,7 +41,30 @@ router.post(
       return res.status(422).json({ errors: errors.array() });
     }
 
-    res.send("auth route");
+    let { name, email, password } = req.body;
+
+    // check if the email exists, it not, save the user
+    User.findOne({ email: email }, (err, user) => {
+      if (err) {
+        return res.status(500).json({ errors: [{ msg: "Database Error" }] });
+      }
+
+      if (user) {
+        return res
+          .status(422)
+          .json({ errors: [{ msg: "Email already exists" }] });
+      }
+
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hash = bcrypt.hashSync(password, salt);
+      req.body.password = hash;
+      User.create(req.body).then(newUser => res.json(newUser));
+    });
+
+    // if not, hash password and save the user
+    // return a token
+
+    //res.send("auth route");
   }
 );
 
--- server.js
+++ server.js
@@ -10,11 +10,6 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
