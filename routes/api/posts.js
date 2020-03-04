95e9bea24ba8c9c8202a6a857ede8475f3b3cbe5
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2020-03-04 13:46:42 -0800

    add routes for like a post and unlike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -89,10 +89,66 @@ router.delete("/:postid", auth, function(req, res) {
 // @route    PUT api/posts/like/:postid
 // @desc     Like a post
 // @access   Private
+router.put("/like/:postid", auth, function(req, res) {
+  Post.findOne({ _id: req.params.postid })
+    .then(post => {
+      if (!post)
+        return res.status(400).json({ errors: [{ msg: "Post not found" }] });
+
+      // each user can only like a post once. If the user already liked the post, return an error
+      let likes = post.likes.map(like => like.user);
+      if (likes.includes(req.userID)) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Post is already liked" }] });
+      }
+
+      return Post.findOneAndUpdate(
+        { _id: req.params.postid },
+        { $push: { likes: { user: req.userID } } },
+        { new: true }
+      );
+    })
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(500).json({ errors: [{ msg: "Database error" }] })
+    );
+});
 
 // @route    PUT api/posts/unlike/:postid
 // @desc     Unlike a post
 // @access   Private
+router.put("/unlike/:postid", auth, function(req, res) {
+  Post.findOne({ _id: req.params.postid })
+    .then(post => {
+      if (!post) {
+        return res.status(400).json({ errors: [{ msg: "Post not found" }] });
+      }
+
+      // If the user hasn't yet liked the post, return an error
+      let likes = post.likes.map(like => like.user);
+      if (!likes.includes(req.userID)) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Post is not yet liked" }] });
+      }
+
+      let newLikes = post.likes.filter(like => {
+        // typeof like.user is object. typeof req.userID is string. So need "toString()" on like.user
+        return like.user.toString() !== req.userID;
+      });
+
+      return Post.findOneAndUpdate(
+        { _id: req.params.postid },
+        { $set: { likes: newLikes } },
+        { new: true }
+      );
+    })
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(500).json({ errors: [{ msg: "Database error" }] })
+    );
+});
 
 // @route    PUT api/posts/comments/:postid
 // @desc     Add comment to a post
