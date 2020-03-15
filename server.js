dd1c4888b28a16b8baba7eb12ba557ca0275d8da
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2020-03-14 23:37:51 -0700

    update server.js to serve static assets from angular build

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/api/auth");
 const userRoute = require("./routes/api/user");
 const profileRoute = require("./routes/api/profile");
 const postsRoute = require("./routes/api/posts");
+const path = require("path");
 
 const app = express();
 
@@ -29,15 +30,19 @@ mongoose.connect(
   }
 );
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Serve up static assets
+app.use(express.static(path.join(__dirname, "dist/dev-connector")));
 
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/profile", profileRoute);
 app.use("/api/posts", postsRoute);
 
-app.get("/", function(req, res) {
-  res.send("Hello world");
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "dist/dev-connector/index.html"));
 });
 
 app.listen(PORT, () => console.log("App is listening on port: " + PORT));
