dedda34f4f704165305851104da06371df2b49f1
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2020-03-18 23:12:11 -0700

    handle logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "../auth.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-navbar",
@@ -9,11 +10,17 @@ import { AuthService } from "../auth.service";
 export class NavbarComponent implements OnInit {
   isAuthed: boolean = false;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     this.authService.isAuthed.subscribe(
       boolValue => (this.isAuthed = boolValue)
     );
   }
 
   ngOnInit() {}
+
+  logout() {
+    localStorage.removeItem("token");
+    this.isAuthed = false;
+    this.router.navigate(["/login"]);
+  }
 }
